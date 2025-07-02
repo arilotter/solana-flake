de90a81c41056bc4cef7385fa795eb923bec865c
Ari Lotter <arilotter@gmail.com>
2025-07-02 14:15:45 -0400

    ghmmm

--- flake.nix
+++ flake.nix
@@ -1,34 +1,42 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
   };
-  outputs = {
-    nixpkgs,
-    ...
-  }: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
-  in {
-    packages = forAllSystems (pkgs: let
-      solana-pkgs = pkgs.callPackage ./default.nix {
-        inherit pkgs;
-      };
-    in {
-      solana = solana-pkgs.solana;
-      solana-rust = solana-pkgs.solana-rust;
-      anchor = solana-pkgs.anchor;
-      default = pkgs.symlinkJoin {
-        name = "solana-all";
-        paths = [
-          solana-pkgs.solana
-          solana-pkgs.solana-rust
-          solana-pkgs.anchor
-        ];
-      };
-    });
-  };
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (
+        pkgs:
+        let
+          solana-pkgs = pkgs.callPackage ./default.nix {
+            inherit pkgs;
+          };
+        in
+        {
+          solana = solana-pkgs.solana;
+          solana-rust = solana-pkgs.solana-rust;
+          anchor = solana-pkgs.anchor;
+          default = pkgs.symlinkJoin {
+            name = "solana-all";
+            paths = [
+              solana-pkgs.solana
+              solana-pkgs.solana-rust
+              solana-pkgs.anchor
+            ];
+          };
+        }
+      );
+    };
 }
